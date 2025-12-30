a08f133d99555cb51db8b5c3ff70ee29cffa4bf5
Saurabh Tomer <tomersaurabh91@gmail.com>
2025-12-30 23:12:25 +0530

    setup auth service

---
+++ auth/src/config/db.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+export const connectDb = async () => {
+  try {
+    await mongoose.connect(process.env.AUTH_MONGO_URL);
+    console.log("Database connected successfully");
+  } catch (error) {
+    console.error("Database connection error:", error.message);
+    process.exit(1); // stop server if DB fails
+  }
+};
---
+++ auth/src/models/user.model.js
@@ -0,0 +1,52 @@
+import mongoose from "mongoose";
+
+
+//sub schema of address
+const addressSchema = new mongoose.Schema({
+  street: String,
+  city: String,
+  state: String,
+  pincode: String,
+  country: String,
+  isDefault:{type:Boolean , default:false}
+
+});
+
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    select:false,
+  },
+  fullName: {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+  },
+  role: {
+    type: String,
+    enum: ["user", "seller"],
+    default: "user",
+  },
+  addresses: [addressSchema],
+});
+
+
+const userModel = mongoose.model('User' , userSchema)
+
+export default userModel
