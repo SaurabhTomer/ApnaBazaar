63358a1768a8677641303913fb65a4bddc15a91a
Saurabh Tomer <tomersaurabh91@gmail.com>
2026-01-06 22:28:14 +0530

    order model

--- order/server.js
+++ order/server.js
@@ -1,16 +1,15 @@
 import dotenv from 'dotenv'
 dotenv.config()
 
-
-import app from "./app.js";
+import app from './src/app.js';
 import { connectDb } from "./src/config/db.js";
 
 
 
-
+const port = process.env.PORT || 3003 
 
 connectDb();
 
 app.listen( port , () => {
-    console.log(`order service running at port ${PORT}`);
+    console.log(`order service running at port ${port}`);
 })
---
+++ order/src/models/order.model.js
@@ -0,0 +1,70 @@
+import mongoose from "mongoose";
+
+
+
+//sub schema of address
+const addressSchema = new mongoose.Schema({
+  street: String,
+  city: String,
+  state: String,
+  pincode: String,
+  country: String,
+  isDefault:{type:Boolean , default:false}
+});
+
+
+
+const orderSchema = new mongoose.Schema({
+
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        required:true
+    },
+    items:[
+        {
+            product:{
+                type:mongoose.Schema.Types.ObjectId,
+                required:true
+            },
+            quantity:{
+                type:Number,
+                default:1,
+                min:1
+            },
+            price:{
+                amount:{
+                    type:Number,
+                    required:true
+                },
+                currency:{
+                    type:String,
+                    required:true,
+                    enum:[ "USD" , "INR"]
+                }
+            }
+        }
+    ],
+    status:{
+        type:String,
+        enum:["PENDING" , "DELIVERED" , "CANCELLED" , "SHIPPED" , "CONFIRMED"]
+    },
+    totalPrice:{
+                amount:{
+                    type:Number,
+                    required:true
+                },
+                currency:{
+                    type:String,
+                    required:true,
+                    enum:[ "USD" , "INR"]
+                }
+    },
+    shippingAddress : addressSchema,
+
+
+} , {timestamps:true})
+
+
+const orderModel = mongoose.model('Order' , orderSchema)
+
+export default orderModel;
